0381578217001b7f438c6290b65c5d212868781d
ekatsevi <katsevich.gene@gmail.com>
2022-07-14 12:43:18 -0400

    Updated sample launch

--- sample_launch.sh
+++ sample_launch.sh
@@ -1,5 +1,5 @@
 source ~/.research_config
-simspec_filename=$LOCAL_SYMCRT_DATA_DIR/private/spec_objects/debug/sim_spec_gaussian_supervised_alternative.rds
+simspec_filename=$LOCAL_SYMCRT_DATA_DIR/private/spec_objects/debug/sim_spec_gaussian_supervised_null.rds
 output_dir=$LOCAL_SYMCRT_DATA_DIR/private/output/debug/
 nextflow run main.nf \
     --simulatr_specifier_fp $simspec_filename \
